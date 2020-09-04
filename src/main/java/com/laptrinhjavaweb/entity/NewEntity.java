bc225ffcda084fe76338fa0282b7c62639a89885
ailamabigio12 <64763859+ailamabigio12@users.noreply.github.com>
2020-09-04 22:37:37 +0700

    spring-mvc

--- src/main/java/com/laptrinhjavaweb/entity/NewEntity.java
+++ src/main/java/com/laptrinhjavaweb/entity/NewEntity.java
@@ -1,5 +1,65 @@
 package com.laptrinhjavaweb.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "new")
 public class NewEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = true, name = "title")
+	private String title;
+	
+	@Column(nullable = true, name = "thumbnail")
+	private String thumbnail;
+	
+	@Column(nullable = true, name = "shortdescription")
+	private String shortDescription;
+	
+	@Column(nullable = true, name = "content")
+	private String content;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
 }
